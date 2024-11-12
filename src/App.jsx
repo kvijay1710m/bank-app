fa2b2a8fd8cdc47f7d085ec449e3d95198207100
kvijay1710 <Vijay@Pixalive.me>
2024-11-12 17:41:37 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { Navbar, Billing, Business, Button, Stats, CardDeal, Clients, CTA, Feedb
 const App = () => {
   return (
     <>
-      <div className='bg-orange-400 w-full overflow-hidden'>
+      <div className='bg-black w-full overflow-hidden'>
         <div className={`${styles.paddingX} ${styles.flexCenter} `}>
           <div className={`${styles.boxWidth}`} >
             <Navbar />
           </div>
         </div>
 
-        <div className={`bg-slate-400 ${styles.flexStart}`}>
+        <div className={`bg-black ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`} >
             <Hero />
           </div>
         </div>
 
-        <div className={`bg-slate-400 ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`} >
             <Stats />
             <Business />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants/index.js'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="logo" className='w-[124px] h-[32px]' />
+
+      {/* Desktop Navigation */}
+      <ul className='list-none hidden sm:flex justify-end items-center flex-1'>{/*hidden->wont come in small screen */}
+        {navLinks.map((nav, index) => (
+          <li key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Menu */}
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        {/* Menu Icon (changes between menu and close icons) */}
+        <img src={toggle ? close : menu}
+          alt="menu"
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        {/* Mobile Nav Links */}
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+              <li key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
